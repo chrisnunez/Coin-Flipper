2a0a752a3bafc00f95e44a4536649bad24ba8bf7
Chrismilez <nunezchristian92@gmail.com>
2022-09-04 17:17:30 -0700

    coinContainer states and link to imgSrc

---
+++ src/CoinContainer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class CoinContainer extends Component {
+  static defaultProps = {
+    coins: [
+      {side: 'heads', imgSrc: "https://tinyurl.com/react-coin-heads-jpg"},
+      {side: 'tails', imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}
+    ]
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currCoin: null,
+      nFlips: 0,
+      nHeads: 0,
+      nTails: 0
+    }
+  }
+
+}
+
+export default CoinContainer;
